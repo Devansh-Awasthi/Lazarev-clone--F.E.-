c00d9c8b5ffb11b3820474897e363951ca879f6a
Devansh-Awasthi <devanshawasthi03@gmail.com>
2024-06-04 12:31:18 +0530

    nav-GSAP-Incomplete

---
+++ script.js
@@ -0,0 +1,38 @@
+var nav = document.querySelector("nav");
+nav.addEventListener("mouseenter", function(){
+    var tl=gsap.timeline();
+    tl.to("#nav-bottom" ,{
+        height:"24vh",
+        duration:"0.5s"
+    })
+    tl.to(".options h5 " ,{
+        display:"block",
+        duration:"1s"
+    })
+    tl.to(".options h5 span", {
+        y: 20,
+         duration:0.3,
+        stagger: {
+            amount: 0.5
+        }
+    })
+})
+nav.addEventListener("mouseleave", function(){
+    var tl1=gsap.timeline();
+  
+    tl1.to(".options h5 " ,{
+        display:"none",
+        duration:"1s"
+    })
+    tl1.to("#nav-bottom" ,{
+        height:"0vh",
+        duration:"0.5s" 
+    })
+    tl1.to(".options h5 span", {
+        y: 25,
+        // duration:0.3,
+        stagger: {
+            amount: 0.5
+        }
+    })
+})
