341dc86f43126370bea3033f07f031f4567279c0
Devansh-Awasthi <devanshawasthi03@gmail.com>
2024-06-05 20:46:57 +0530

    page2-img-animation(only right side)

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+function p1animation(){
 var nav = document.querySelector("nav");
 nav.addEventListener("mouseenter", function(){
     var tl=gsap.timeline();
@@ -36,3 +37,34 @@ nav.addEventListener("mouseleave", function(){
     })
     
 })
+}
+
+function p2animation(){
+var ins=document.querySelectorAll(".inside");
+ins.forEach(function(a){
+    a.addEventListener("mouseenter", function(){
+        // console.log(a.childNodes  );
+        gsap.to(a.childNodes[7],{
+            opacity:1,
+            scale:1
+        })
+        
+    })
+    a.addEventListener("mouseleave", function(){
+        // console.log(a.childNodes  );
+        gsap.to(a.childNodes[7],{
+            opacity:0,
+            scale:0
+        })
+    a.addEventListener( "mousemove", function(dets){    
+        gsap.to(a.childNodes[7],{
+            x:dets.x - a.getBoundingClientRect().x-35,
+            y: dets.y - a.getBoundingClientRect().y-120 ,            
+        })
+    })
+})
+})
+
+}
+p1animation();
+p2animation();
