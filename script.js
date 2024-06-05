2667b065e9c3ad7c4d74629909bedf6d6581ae46
Devansh-Awasthi <devanshawasthi03@gmail.com>
2024-06-05 11:50:49 +0530

    reel complete

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var nav = document.querySelector("nav");
 nav.addEventListener("mouseenter", function(){
     var tl=gsap.timeline();
     tl.to("#nav-bottom" ,{
-        height:"26vh",
+        height:"20vh",
         duration:"0.5s"
     })
     tl.to(".options h5 " ,{
@@ -19,7 +19,13 @@ nav.addEventListener("mouseenter", function(){
 })
 nav.addEventListener("mouseleave", function(){
     var tl1=gsap.timeline();
-  
+    tl1.to(".options h5 span", {
+        y: 25,
+        // duration:0.3,
+        stagger: {
+            amount: 0.5
+        }
+    })
     tl1.to(".options h5 " ,{
         display:"none",
         duration:"0.1s"
@@ -28,11 +34,5 @@ nav.addEventListener("mouseleave", function(){
         height:"0vh",
         duration:"0.3s"
     })
-    tl1.to(".options h5 span", {
-        y: 25,
-        // duration:0.3,
-        stagger: {
-            amount: 0.5
-        }
-    })
+    
 })
