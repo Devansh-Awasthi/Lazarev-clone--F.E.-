33b174fb6dc6758ee3de504db8bebee4a91671f0
Devansh-Awasthi <devanshawasthi03@gmail.com>
2024-06-16 02:02:54 +0530

    final

--- script.js
+++ script.js
@@ -1,55 +1,4 @@
-// // function arrow(){
-// //     var ar = document.querySelector(".inside");
-// //     ar.addEventListener("mouseenter", function(){
-// //         var tl=gsap.timeline();
-// //         tl.to(".inside #hidden",{
-// //             // y: 0,
-// //             display:"inline-block",    
-// //             duration:"0.5s"
-// //         })
-// //     })
-// // }
-// // arrow();
-// function locomotive(){
-//     gsap.registerPlugin(ScrollTrigger);
-// const locoScroll = new LocomotiveScroll({
-//   el: document.querySelector("#main"),
-//   smooth: true,
 
-//   // for tablet smooth
-//   tablet: { smooth: true },
-
-//   // for mobile
-//   smartphone: { smooth: true }
-// });
-// locoScroll.on("scroll", ScrollTrigger.update);
-
-// ScrollTrigger.scrollerProxy("#main", {
-//   scrollTop(value) {
-//     return arguments.length
-//       ? locoScroll.scrollTo(value, 0, 0)
-//       : locoScroll.scroll.instance.scroll.y;
-//   },
-//   getBoundingClientRect() {
-//     return {
-//       top: 0,
-//       left: 0,
-//       width: window.innerWidth,
-//       height: window.innerHeight
-//     };
-//   }
-
-//   // follwoing line is not required to work pinning on touch screen
-
-//   /* pinType: document.querySelector(".smooth-scroll").style.transform
-//     ? "transform"
-//     : "fixed"*/
-// });
-// ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-// ScrollTrigger.refresh();
-
-// }
 function p1animation(){
 var nav = document.querySelector("nav");
 nav.addEventListener("mouseenter", function(){
@@ -99,10 +48,10 @@ function loadingAnimation() {
  tl.from("#p1",{
     transform:"scaleX(0.7) scaleY(0.2) translateY(80%)",
     borderRadius:"200px",
-    duration:2,
+    duration:1,
     ease:"expo.out",
  })
-tl.from("#p1 ,#p1 h1,#p1 div,#p1 p",{
+tl.from("#p1 ,nav,#p1 h1,#p1 div,#p1 p",{
     opacity:0,
     duration:0.5,
     stagger: 0.2,
@@ -157,52 +106,7 @@ vid.addEventListener("click", function(){
 })    
 
 }
-// function p4animationd3(){
-//     var covers = document.querySelectorAll(".d3-right1");
 
-//     covers.forEach(function(cover) {
-//       var vid = cover.querySelector("video");
-      
-//       cover.addEventListener("mouseenter", function() {
-//         vid.play();
-//         gsap.to(vid, {
-//           opacity: 1,
-       
-//         });
-//       });
-  
-//       cover.addEventListener("mouseleave", function() {
-//         vid.pause();
-//         gsap.to(vid, {
-//             currentTime: 0
-//         });
-//       });
-//     });
-  
-// }
-// function p4animationcase3(){
-//     var covers = document.querySelectorAll(".new-vid");
-//     covers.forEach(function(cover) {
-//       var vid = cover.querySelector("video");
-
-//       cover.addEventListener("mouseenter", function() {
-//         vid.play();
-
-//         gsap.to(cover, {
-//           height:"80vh",
-//           currentTime: 0
-//         });
-//       });
-  
-//       cover.addEventListener("mouseleave", function() {
-//         vid.pause();
-//            gsap.to(cover, {
-//              height: "42vh",
-//           currentTime: 0
-//         });
-//       });
-//     });
-// }
 document.addEventListener('DOMContentLoaded', function() {
     gsap.registerPlugin(ScrollTrigger);
 
@@ -276,64 +180,7 @@ document.addEventListener('DOMContentLoaded', function() {
     p6scrollanimation();
 });
 
-//  function p7scrollanimation(){
-//     gsap.to(".p7-right", {
-//         scrollTrigger: {
-//           trigger: ".p7-left",
-//           start: "top 10%", // When the top of .p7-left hits the top of the viewport
-//           end: "bottom 50%", // When the bottom of .p7-left hits the top of the viewport
-//           pin: true,
-//           scrub: true,
-//         //   markers: true, // Remove this line to hide markers
-//         }
-//       });
-//  }
-//  function p7_2023animation() {
-//     document.addEventListener('DOMContentLoaded', function() {
-//         gsap.registerPlugin(ScrollTrigger);
-//         ScrollTrigger.create({
-//           trigger: ".t-p-left",
-//           start: "top 20%",
-//           end: "bottom 28%",
-//           pin: ".t-p-left button",
-//         //   markers: true // Optional: Show markers for debugging
-//         });
-//       });
-//     var panels = document.querySelectorAll("#panel1");
 
-//     panels.forEach(function(panel) {
-//         var img = panel.querySelector("#panel1>img");
-        
-//         panel.addEventListener("mouseenter", function() {
-//             gsap.to(img, {
-//                 opacity: 1,
-//                 scale: 1,
-//                 display: "block"
-//             });
-//         });
-
-//         panel.addEventListener("mouseleave", function() {
-//             gsap.to(img, {
-//                 opacity: 0,
-//                 scale: 0,
-//                 display: "none"
-//             });
-//         });
-
-//         panel.addEventListener("mousemove", function(event) {
-//             var rect = panel.getBoundingClientRect();
-//             var x = event.clientX - rect.left - (img.width / 2);
-//             var y = event.clientY - rect.top - (img.height / 2);
-//             var angle = (event.clientX - rect.left) / rect.width * 60 - 30; // -30 to 30 degrees
-
-//             gsap.to(img, {
-//                 x: x * 0.3, // Move less by applying a fraction
-//                 y: y * 0.1, // Move less by applying a fraction
-//                 rotate: angle*0.05, // Apply calculated rotation angle
-//             });
-//         });
-//     });
-// }
 // locomotive();
 // p4animationcase3();
 // p4animationd3();
@@ -341,8 +188,8 @@ p3animation();
 loadingAnimation();
 // // p7scrollanimation();
 // // p7_2023animation();
-// p1animation();
-// p2animation();
+p1animation();
+p2animation();
 document.addEventListener('DOMContentLoaded', function() {
     gsap.registerPlugin(ScrollTrigger);
 
